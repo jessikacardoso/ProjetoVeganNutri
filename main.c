f52428f875d214d4619fc1c92e60cbc8bc519202
jessikacardoso <jessika.nascimento@a.ucb.br>
2025-10-15 20:54:43 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Projeto: Vegan Nutri
+Autor: Gustavo Alexandre e Jessika Cardoso
+Data de criação: 15/10/2025
+Descrição:Sistema de substituições veganas.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Iniciando o Projeto: Vegan Nutri...\n");
+    return 0;
+}
